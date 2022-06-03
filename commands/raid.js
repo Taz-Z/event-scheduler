489f9fdfbf3fd355347603e0b8b56622d20e37a0
Taz-Z <tanzim_zaman@hotmail.com>
2022-06-03 02:16:05 -0400

    Added options to raid

--- commands/raid.js
+++ commands/raid.js
@@ -7,10 +7,22 @@ module.exports = {
     .setDescription("Sets up a raid embed!")
     .addStringOption((option) =>
       option
-        .setName("Raid Type")
+        .setName("content")
         .setDescription("The raid which you are doing")
         .setRequired(true)
         .addChoices(
+          {
+            name: "Argos P1",
+            value: "Argos P1",
+          },
+          {
+            name: "Argos P2",
+            value: "Argos P2",
+          },
+          {
+            name: "Argos P3",
+            value: "Argos P3",
+          },
           {
             name: "Normal Valtan",
             value: "Normal Valtan",
@@ -18,11 +30,52 @@ module.exports = {
           {
             name: "Hard Mode Valtan",
             value: "Hard Mode Valtan",
+          },
+          {
+            name: "Normal Vykas",
+            value: "Normal Vykas",
+          },
+          {
+            name: "Hard Mode Vykas",
+            value: "Hard Mode Vykas",
+          }
+        )
+    )
+    .addStringOption((option) =>
+      option
+        .setName("date")
+        .setDescription("Date and time of raid")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("leader")
+        .setDescription("Name of raid leader")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("role")
+        .setDescription("Are you a dps or support?")
+        .setRequired(true)
+        .addChoices(
+          {
+            name: "I am a dps",
+            value: "dps",
+          },
+          {
+            name: "I am a support",
+            value: "supp",
           }
         )
     ),
-
   async execute(interaction) {
+    const content = interaction.options.getString("content");
+    const date = interaction.options.getString("date");
+    const leader = interaction.options.getString("leader");
+    const dps = interaction.options.getString("role");
+    console.log(content, date, leader, dps);
+    if (!content || !date || !leader || !role) return;
     const embed = getBaseEmbed("Uhhh");
     await interaction.reply({ embeds: [embed] });
   },
